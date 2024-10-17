c26c1d6bc1ba3fc53041cae9cbe09ff4cf5e41ca
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-17 19:36:38 -0400

    saarted project and added, also created custom user model

---
+++ accounts/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+)
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """Custome User Creaton Form"""
+
+    class Meta:
+        """This is the Meta class. it contains information"""
+
+        # about how to get an instance of class created.
+        # it is not common to se this, but Django dose for
+        # certain things. Like this
+
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("age",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """Custome User Change Form"""
+
+    class Meta:
+        """meta"""
+
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
