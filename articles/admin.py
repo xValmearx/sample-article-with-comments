78c6ecd1f24dca86f9f8371bd108b0b341544556
Caleb Taylor <calebs.phone2017@gmail.com>
2024-11-05 20:13:25 -0500

    created comment functionality to the apps, can write and view comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInLine(admin.StackedInline):
+    """Comment InLine for Articles Admin"""
+
+    # could sue admin.StackedInlIne as the class to inherit form
+    # if we wanted a different looking inline that is "stacked" vs "horizontal"
+
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    """Article Admin"""
+
+    inlines = [CommentInLine]
 
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -21,3 +21,27 @@ class Article(models.Model):
     def get_absolute_url(self):
         """Get the absolute url for the model"""
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    """Comment Model"""
+
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+
+    text = models.CharField(max_length=140)
+
+    def __str__(self):
+        """string method"""
+        return self.text
+
+    def get_absolute_url(self):
+        """get the absolute url for the comment"""
+
+        # would be better to have an actual detail view for the comment
+        return reverse("article_list")
