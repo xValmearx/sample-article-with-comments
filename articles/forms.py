2a7b56bed72195817021817997400ab689947d10
Caleb Taylor <calebs.phone2017@gmail.com>
2024-11-07 19:43:34 -0500

    created comment functionality to the project, can now leave comments on peoples post

---
+++ articles/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    """Comment Form"""
+
+    class Meta:
+        model = Comment
+        fields = ("text", "author")
--- articles/views.py
+++ articles/views.py
@@ -2,9 +2,19 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin,
 )
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.http import HttpResponse
 
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, FormView
+
+from django.views.generic.detail import SingleObjectMixin
+
+from django.urls import reverse_lazy, reverse
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.views import View
+
+from .forms import CommentForm
 
 from .models import Article
 
@@ -18,11 +28,55 @@ class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "article_list.html"
 
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+class ArticleDetailView(LoginRequiredMixin, View):
     """Article Detial View"""
+
+    def get(self, request, *args, **kwargs):
+        """Get Request"""
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """Post Request"""
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
+
+class CommentGet(LoginRequiredMixin, DetailView):
+    """Article Detial View / Add Comment Form"""
 
     model = Article
     template_name = "article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        """Get contex data"""
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    """Comment Post"""
+
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        """handle post request"""
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("article_detail", kwargs={"pk": article.pk})
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
